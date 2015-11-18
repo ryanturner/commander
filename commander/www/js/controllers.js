b8c9ab5659da26d877aa91ba9ef652b2c57f3496
Ryan Turner <ryan.e.t@gmail.com>
2015-11-18 12:52:42 -0600

    More progress

--- commander/www/js/controllers.js
+++ commander/www/js/controllers.js
@@ -1,11 +1,26 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova', 'ui.router'])
 
-.controller('DashCtrl', function($scope) {
-  $scope.items = ["Test", "One", "Two"];
+.controller('DashCtrl', function($scope, $ionicPlatform, $cordovaBluetoothSerial, $state) {
+  $scope.items = [{"id":0,"label":"Test"},{"id":0,"label":"Test"},{"id":0,"label":"Test"}];
   $scope.listCanSwipe = true;
   $scope.shouldShowReorder = true;
   $scope.shouldShowDelete = true;
-
+  $ionicPlatform.ready(function() {
+    console.log("ready");
+    $scope.liste = function() {
+      $cordovaBluetoothSerial.list().then(function(result){
+        $scope.btlist = result;
+        console.log(result);
+        console.log("yay");
+      },
+      function(error) {
+        console.log(error);
+      });
+    }
+  });
+  $scope.go = function(path) {
+    $state.go(path);
+  };
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -31,4 +46,5 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
+  $scope.bluetoothDevices = [{"name":"test device"}, {"name":"test device 2"}];
 });
