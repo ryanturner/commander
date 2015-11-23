75dce2cc93bf7ea957e9c1515649136c02680047
Ryan Turner <rturns@SC-RTURNER-IOS.local>
2015-11-23 17:10:07 -0600

    Got gulp setup to now run a single test if you do gulp test. yay

--- commander/gulpfile.js
+++ commander/gulpfile.js
@@ -6,6 +6,8 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var gulp = require('gulp');
+var Server = require('karma').Server;
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -48,4 +50,11 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/tests/unit-tests.conf.js',
+    singleRun: true
+  }, done).start();
 });
